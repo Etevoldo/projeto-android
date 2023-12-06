5de3f52557a9519626491635d5b597de26517db9
voldo <filipecavoldo@outlook.com>
2023-12-06 18:40:51 -0300

    parte visual feita

--- app/src/main/java/com/example/portfolio_android/pedidosAdm.java
+++ app/src/main/java/com/example/portfolio_android/pedidosAdm.java
@@ -1,14 +1,42 @@
 package com.example.portfolio_android;
 
+import static com.example.portfolio_android.FuncoesAjuda.showToast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.TextView;
 
-public class pedidosAdm extends AppCompatActivity {
+public class pedidosAdm extends AppCompatActivity implements View.OnClickListener {
 
+    String ra, nome, email, senha;
+    int numeroDePedidos;
+    EditText edtEntregaConfirma;
+    Button btnConfEntrega;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedidos_adm);
+
+        Intent intent = getIntent();
+        Bundle parametros = intent.getExtras();
+        ra = parametros.getString("ra");
+        nome = parametros.getString("nome");
+        email = parametros.getString("email");
+        senha = parametros.getString("senha");
+        numeroDePedidos = parametros.getInt("numeroDePedidos");
+
+        edtEntregaConfirma = (EditText) findViewById(R.id.edtEntregaConfirma);
+        btnConfEntrega = (Button) findViewById(R.id.btnConfEntrega);
+
+        btnConfEntrega.setOnClickListener(this);
+    }
+
+    public void onClick(View view){
+        showToast("something lol", getBaseContext());
     }
 }
