707c44598e79f0fe373536d222350747c5c34068
voldo <filipecavoldo@outlook.com>
2023-12-06 19:10:41 -0300

    modificado classe pedido para ser de acordo com a nova db

--- app/src/main/java/com/example/portfolio_android/pedidosAdm.java
+++ app/src/main/java/com/example/portfolio_android/pedidosAdm.java
@@ -5,11 +5,16 @@ import static com.example.portfolio_android.FuncoesAjuda.showToast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class pedidosAdm extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,6 +22,7 @@ public class pedidosAdm extends AppCompatActivity implements View.OnClickListene
     int numeroDePedidos;
     EditText edtEntregaConfirma;
     Button btnConfEntrega;
+    ListView lista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +38,38 @@ public class pedidosAdm extends AppCompatActivity implements View.OnClickListene
 
         edtEntregaConfirma = (EditText) findViewById(R.id.edtEntregaConfirma);
         btnConfEntrega = (Button) findViewById(R.id.btnConfEntrega);
+        lista = (ListView) findViewById(R.id.listPedidos);
 
         btnConfEntrega.setOnClickListener(this);
+
+        List<pedido> listaPedidos;
+        listaPedidos = consultarPedidos();
+
+        PedidosListaAdapter adaptador = new PedidosListaAdapter(this, listaPedidos);
+        listaPedidos.setAdapter(adaptador);
+
+    }
+
+    public List<pedido> consultarPedidos(){
+        List<pedido> lista= new LinkedList<pedido>();
+        BancoController bd = new BancoController(getBaseContext());
+        Cursor dados = bd.consultarTodosPedido();
+
+        if (dados != null) {
+            do {
+                pedido item = new pedido();
+                item.setIdPedido(dados.getInt(0));
+                item.setIdExemplar(dados.getInt(1));
+                item.setRa(dados.getString(2));
+                item.setDataEntrega(dados.getString(3));
+                item.setIdPedido(dados.getInt(4));
+                lista.add(item);
+            } while (dados.moveToNext()); //retorna 0 quando acaba
+        }
+        else {
+            showToast("Não há nenhum exemplar cadastrado", getBaseContext());
+        }
+        return lista;
     }
 
     public void onClick(View view){
